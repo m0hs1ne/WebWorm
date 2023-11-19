ff7d72ca25f960c29ba743676514c3b94f795b72
m0hs1ne <mel-hada@student.1337.ma>
2023-11-19 16:55:34 +0100

    Add results directory to gitignore and improve code formatting

--- scraper.py
+++ scraper.py
@@ -8,6 +8,7 @@ GREEN = "\033[92m"
 YELLOW = "\033[93m"
 RESET = "\033[0m"
 
+
 class WebScraper:
     def __init__(self, url, depth, extensions=None):
         self.url = url
@@ -17,7 +18,10 @@ class WebScraper:
         self.downloaded_files = set()
 
     def is_valid_url(self, url):
-        return(urlparse(url).netloc == urlparse(self.url).netloc and url not in self.visited_urls)
+        return (
+            urlparse(url).netloc == urlparse(self.url).netloc
+            and url not in self.visited_urls
+        )
 
     def get_page_content(self, url):
         try:
@@ -27,22 +31,21 @@ class WebScraper:
         except requests.exceptions.HTTPError as e:
             print(f"{RED}HTTP error occurred: {e}{RESET}")
             return None
-    
-    def download_file(self, url, download_dir='results'):
+
+    def download_file(self, url, download_dir="results"):
         os.makedirs(download_dir, exist_ok=True)
         filename = os.path.basename(urlparse(url).path)
         file_path = os.path.join(download_dir, filename)
         try:
             with requests.get(url) as response:
                 response.raise_for_status()
-                with open(file_path, 'wb') as file:
+                with open(file_path, "wb") as file:
                     for chunk in response.iter_content(chunk_size=8192):
                         file.write(chunk)
                     print(f"{GREEN}Downloaded file: {url}{RESET}")
         except requests.exceptions.HTTPError as e:
             print(f"{RED}HTTP error occurred: {e}{RESET}")
-                    
-    
+
     def extract_files(self, soup, url):
         links_attributes = ["href", "src"]
         for tag in soup.find_all(True, href=True) + soup.find_all(True, src=True):
@@ -50,10 +53,15 @@ class WebScraper:
                 if tag.has_attr(attribute):
                     link = tag[attribute]
                     parsed_link = urlparse(link)
-                    if '.' in os.path.basename(parsed_link.path):
-                        if not self.extensions or any([link.endswith(ext) for ext in self.extensions]):
+                    if "." in os.path.basename(parsed_link.path):
+                        if not self.extensions or any(
+                            [link.endswith(ext) for ext in self.extensions]
+                        ):
                             file_url = urljoin(url, link)
-                            if self.is_valid_url(file_url) and file_url not in self.downloaded_files:
+                            if (
+                                self.is_valid_url(file_url)
+                                and file_url not in self.downloaded_files
+                            ):
                                 self.download_file(file_url)
                                 self.downloaded_files.add(file_url)
 
@@ -63,7 +71,6 @@ class WebScraper:
             if self.is_valid_url(link):
                 self.scrape_page(link, current_depth + 1)
 
-
     def scrape_page(self, url, current_depth):
         if current_depth > self.depth or not self.is_valid_url(url):
             return
@@ -71,11 +78,13 @@ class WebScraper:
         content = self.get_page_content(url)
         if content is None:
             return
-        
+
         soup = BeautifulSoup(content, "html.parser")
-        self.extract_files(soup,url)
+        self.extract_files(soup, url)
         self.crawl_to_links(soup, url, current_depth)
 
     def start_scraping(self):
         self.scrape_page(self.url, current_depth=1)
-        print(f"{YELLOW}Total unique files downloaded: {len(self.downloaded_files)}{RESET}")
+        print(
+            f"{YELLOW}Total unique files downloaded: {len(self.downloaded_files)}{RESET}"
+        )
